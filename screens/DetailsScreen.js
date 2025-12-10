e3821db873c5198be763aa4428a48b48b8c8f594
info <mary4maf@gmail.com>
2025-12-10 21:30:53 +0100

    Premier commit - projet React Native Expo

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function DetailsScreen({ route }) {
+  return (
+    <View style={styles.container}>
+      <Text>📄 Écran de détails</Text>
+      {route.params && <Text>ID reçu : {route.params.id}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+});
